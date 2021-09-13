0c62bfd8eda49bf872330190bc137d2375ff67b1
xerx <85109217+equitiinferos@users.noreply.github.com>
2021-09-13 15:59:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import pandas as pd
 import xlsxwriter
-from binance.client import Client
+#dataSource
+import yfinance as yf
 
-#Create a workbook and add a worksheet
+#create a workbook and add a worksheet
 workbook = xlsxwriter.Workbook('Stocks.xlsx')
 worksheet = workbook.add_worksheet()
 
-#Data Source
-import yfinance as yf
+#cryptoData
+coins = ['BTC-EUR', 'ETH-EUR', 'ADA-EUR', 'VET-EUR']
+cryptoData = yf.download(tickers=coins, period='1d', interval='1d')
+#clean cryptoData
+columns = ["Adj Close", "High", "Low", "Open", "Volume"]
+cryptoData.drop(columns=columns, axis=1, inplace=True)
+cryptoData = cryptoData.T
 
 #stocksData
-stocks = ['AAPL', 'MSFT', 'NEE', 'AMGN', 'TU', 'ZM', 'TSLA', 'TTD', 'SEDG', 'APPN', 'TWLO', 'MMM', 'ABBV', 'SHW', 'K', 'HON', 'AFL', 'JNJ', 'ABT', 'EMR', 'FSLR', 'SPWR', 'ENPH']
-stockNames = ['Apple', 'Microsoft', 'NextEra Energy', 'Amgen', 'Telus', 'Zoom Video', 'Tesla', 'The Trade Desk', 'SolarEdge Technologies', 'Appian', 'Twilio', '3M Company', 'AbbVie Inc', 'Sherwin Williams', 'Kellogg Co', 'Honeywell', 'Aflac', 'Johnson & Johnson', 'Abbott Labs', 'Emerson Electric', 'First Solar, Inc.', 'SunPower Corporation',  'Enphase  Energy, Inc' ]
+stocks = ['AAPL', 'MSFT', 'NEE', 'AMGN', 'TU', 'ZM', 'TSLA', 'TTD', 'SEDG', 'APPN', 'TWLO', 'MMM', 'ABBV', 'SHW', 'K', 'HON', 'AFL', 'JNJ', 'ABT', 'EMR', 'FSLR', 'SPWR', 'ENPH', '^GSPC']
+stockNames = ['Apple', 'AbbVie Inc', 'Abbott Labs', 'Aflac', 'Amgen', 'Appian', 'Emerson Electric', 'Enphase  Energy, Inc', 'First Solar, Inc.', 'Honeywell', 'Johnson & Johnson', 'Kellogg Co', '3M Company', 'Microsoft', 'NextEra Energy', 'SolarEdge Technologies', 'Sherwin Williams', 'SunPower Corporation', 'Tesla', 'The Trade Desk', 'Telus', 'Twilio', 'Zoom Video', 'S&P500']
 data = yf.download(tickers=stocks, period='1d', interval='1d')
-
-print(data)
-
-#clean data
+#clean stockData
 columns = ["Adj Close", "High", "Low", "Open", "Volume"]
 data.drop(columns=columns, axis=1, inplace=True)
+data.index = data.index.tz_localize(None)#Remove timezone
+data = data.T#Swap Rows/Columns
 
-#remove timezone
-data.index = data.index.tz_localize(None)
-
-#swap Rows/Columns
-data = data.T
-
-#append column
-data[""] = ""
+#appendColumn
 data['Stocks'] = stockNames
 
-#send stocksData to excel
+#concatonateData
+df2 = pd.DataFrame(cryptoData)
+data = pd.concat([data, df2], ignore_index=False)
+
+#Send data to workbook
 datatoexcel = pd.ExcelWriter("Stocks.xlsx", engine='xlsxwriter')
 data.to_excel(datatoexcel, sheet_name='Sheet1')
 datatoexcel.save()
+
+print(data)
